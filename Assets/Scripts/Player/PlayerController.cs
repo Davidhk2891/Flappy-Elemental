39742773766b7bfa122574919be2f0aae986be8e
David Rajchenberg <davidrajchenberg@Mac.lan>
2025-10-24 23:20:05 +0300

    Features added: 1) Floor and Ceiling 2) sensors to Floor and Ceiling 3) Restart scene method for GameOver (restarts game for now) 4) Background, some colors

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class PlayerController : MonoBehaviour
         // Get the Rigidbody 2D component attached to this GameObject
         rigidBody = GetComponent<Rigidbody2D>();
     }
-    
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -19,5 +20,29 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Jumped");
             rigidBody.linearVelocity = Vector2.up * flapStrength;
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Player collided with; " + collision.gameObject.name);
+        GameOver();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Log for debuging
+        Debug.Log($"Player entered trigger: {other.gameObject.name}");
+
+        // Check if trigger has tag Boundry
+        if (other.gameObject.CompareTag("Boundary"))
+        {
+            Debug.Log("Player hit floor or ceiling. Restarting game");
+            GameOver();
+        }
+    }
+    
+    void GameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
