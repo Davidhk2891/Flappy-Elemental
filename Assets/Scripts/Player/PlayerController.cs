6f0f8693b6569e6a457952bf1865de741f36fcd4
David Rajchenberg <davidrajchenberg@Davids-MacBook-Air.local>
2025-11-10 20:58:59 +0200

    added kirby sprite

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
-    private Rigidbody2D rigidBody;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
     public float flapStrength = 5f;
+
+    [Header("Kirby Sprites")]
+    public Sprite kirbyIdle;
+    public Sprite kirbyFlap;
+    public float flapDuration = 0.15f;
 
     void Start()
     {
         // Get the Rigidbody 2D component attached to this GameObject
-        rigidBody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        sr.sprite = kirbyIdle;
     }
 
     void Update()
@@ -17,9 +26,16 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Listens for space bar press. When pressed, y * flapStrength
-            Debug.Log("Jumped");
-            rigidBody.linearVelocity = Vector2.up * flapStrength;
+            rb.linearVelocity = Vector2.up * flapStrength;
+            StartCoroutine(FlapAnimation());
         }
+    }
+
+    private IEnumerator FlapAnimation()
+    {
+        sr.sprite = kirbyFlap;
+        yield return new WaitForSeconds(flapDuration);
+        sr.sprite = kirbyIdle;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
